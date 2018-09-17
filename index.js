0a0bdaa28b72b77a09e69361768598537748bd6e
Blair Stuart <blaircstuart28@gmail.com>
2018-09-17 18:52:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-function add () {
-  
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+
+function add (a,b) {
+
 }
 
 function subtract (a,b) {
   
 }
 
-function multiply (){
+function multiply (a,b){
   
 }
 
-function divide (){
+function divide (a,b){
   
 }
 

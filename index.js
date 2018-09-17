19aa4288e72e2ee50b0996bd794bc5099606b5b5
Blair Stuart <blaircstuart28@gmail.com>
2018-09-17 19:11:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function dec(a){
 
  var a = Math.floor(Math.random() * 1000)
  var b = Math.floor(Math.random() * 1000)
-function makeInt(a){
+function makeInt(){
   parseInt('a' , 10)
   return a
 }
